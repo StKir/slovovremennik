7b804844a527c74ede6963532052db69e10f97a2
Даниил Мухреев <muxreev.dan2@yandex.ru>
2023-05-11 23:26:41 +0300

    feat(component): :zap: Добавлен компонент модальное окно и inputs

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -2,16 +2,18 @@ import React, { FC, PropsWithChildren } from 'react';
 import Header from '../ui/header/Header';
 import Backgrounds from '../ui/background/Backgrounds';
 import Footer from '../ui/footer/Footer';
+import { FormModalsAuth } from '../ui/modals';
 
 const MainLayout: FC<PropsWithChildren> = ({ children }) => {
-	return (
-		<>
-			<Header />
-			<Backgrounds />
-			{children}
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <Backgrounds />
+      <FormModalsAuth />
+      {children}
+      <Footer />
+    </>
+  );
 };
 
 export default MainLayout;
---
+++ src/components/ui/modals/constant.ts
@@ -0,0 +1,32 @@
+import { IConstantValesFromInput } from './types';
+
+export const valuesForAuth: Array<IConstantValesFromInput> = [
+  {
+    name: 'email',
+    title: 'Почта',
+    type: 'text',
+  },
+  {
+    name: 'password',
+    title: 'Пароль',
+    type: 'password',
+  },
+];
+
+export const valuesForReg: Array<IConstantValesFromInput> = [
+  {
+    name: 'nikname',
+    title: 'Никнейм',
+    type: 'text',
+  },
+  {
+    name: 'email',
+    title: 'Почта',
+    type: 'text',
+  },
+  {
+    name: 'password',
+    title: 'Пароль',
+    type: 'password',
+  },
+];
