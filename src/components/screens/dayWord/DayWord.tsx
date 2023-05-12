714068064348f824c3b0bf7e201feab9ca160de1
Даниил Мухреев <muxreev.dan2@yandex.ru>
2023-05-12 16:30:56 +0300

    feat: :art: Страница слово дня

--- src/components/screens/dayWord/DayWord.tsx
+++ src/components/screens/dayWord/DayWord.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import styles from './dayWord.module.scss';
 
 const DayWord = () => {
-	return <div>word</div>;
+  return (
+    <div>
+      <h3>Слово дня</h3>
+      <div>
+        <h2>Килиманджарит</h2>
+        <p>Состоянии безумной эйфории, неописуемого кайфа</p>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <h4>Часть речи</h4>
+          </li>
+          <li>
+            <h4>Теги</h4>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <p>
+              - Брат когда вижу пейзаж родного аула меня жестко килиманджарит
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default DayWord;
