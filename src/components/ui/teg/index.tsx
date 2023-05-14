71548c82e1dd2fe7ed0c6f6bbd46cdc2bb45e0ef
Kirill <kirillstrukov2@gmail.com>
2023-05-14 14:23:09 +0300

    first adapt

--- src/components/ui/teg/index.tsx
+++ src/components/ui/teg/index.tsx
@@ -2,12 +2,12 @@ import clsx from 'clsx';
 import styles from './tegButton.module.scss';
 
 interface ITeg {
-  children: React.ReactElement | React.ReactNode;
-  type: 'blue' | 'pink';
+	children: React.ReactElement | React.ReactNode;
+	type: 'blue' | 'pink';
 }
 
 const Teg: React.FC<ITeg> = ({ children, type }) => {
-  return <div className={clsx(styles.teg, styles[type])}>{children}</div>;
+	return <div className={clsx(styles.teg, styles[type])}>{children}</div>;
 };
 
 export default Teg;
