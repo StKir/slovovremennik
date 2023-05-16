279eaa3483168c59fac1c68a84ef26c7437076a2
Даниил Мухреев <muxreev.dan2@yandex.ru>
2023-05-16 23:42:38 +0300

    feat: :zap: Добавлена тема для кнопки

--- src/components/ui/input/mainInput/index.tsx
+++ src/components/ui/input/mainInput/index.tsx
@@ -1,8 +1,13 @@
 import { IMainInput } from '@/interfaces/ui.interface';
 import styles from './mainInput.module.scss';
 
-const MainInput: React.FC<IMainInput> = ({ ...props }) => {
-	return <input className={styles.input} {...props} />;
+const MainInput: React.FC<IMainInput> = ({ typeTheme = 'primary', ...props }) => {
+  return (
+    <input
+      className={typeTheme === 'primary' ? styles.input_primary : styles.input_secondary}
+      {...props}
+    />
+  );
 };
 
 export default MainInput;
