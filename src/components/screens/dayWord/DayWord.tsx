560cb0828fee81dcf3467c3078c0db0d3aaec416
Даниил Мухреев <muxreev.dan2@yandex.ru>
2023-05-16 21:35:32 +0300

    fix: :art: Изменения нейминга

--- src/components/screens/dayWord/DayWord.tsx
+++ src/components/screens/dayWord/DayWord.tsx
@@ -19,50 +19,46 @@ interface IDemo {
 const RenderContentDayWord: React.FC<{ data: IWord }> = ({ data }) => {
   return (
     <>
-      <div>
-        <div>
-          <h2 className={styles.main_title}>{data.word}</h2>
-          <p className={styles.main_direction}>Состоянии безумной эйфории, неописуемого кайфа</p>
-        </div>
+      <h2 className={styles.main_title}>{data.word}</h2>
+      <p className={styles.main_direction}>{data.description}</p>
+      <div className={styles.info_block}>
         <div className={styles.info_content}>
-          <div className={styles.info_section}>
-            <ul className={styles.list_info}>
-              <li className={styles.item_info}>
-                <h4>Часть речи</h4>
-                <p>{data.speech}</p>
-              </li>
-              <li className={styles.item_info}>
-                <h4>Теги</h4>
-                <div className={styles.tegs_content}>
-                  {data.tegs.map((el, index) => (
-                    <Teg key={index} type={index % 2 === 0 ? 'pink' : 'blue'}>
-                      {el}
-                    </Teg>
-                  ))}
-                </div>
-              </li>
-            </ul>
-            <h4 className={styles.info_author}>Автор: {data.author}</h4>
-
-            <div className={styles.likes_func}>
-              <div className={clsx(styles.btn_action, styles.like)}>
-                <Image src={Like} alt="like" />
+          <ul className={styles.list_info}>
+            <li className={styles.item_info}>
+              <h4>Часть речи</h4>
+              <p>{data.speech}</p>
+            </li>
+            <li className={styles.item_info}>
+              <h4>Теги</h4>
+              <div className={styles.tegs_content}>
+                {data.tegs.map((el, index) => (
+                  <Teg key={index} type={index % 2 === 0 ? 'pink' : 'blue'}>
+                    {el}
+                  </Teg>
+                ))}
               </div>
-              <div className={clsx(styles.btn_action, styles.dislike)}>
-                <Image src={DisLike} alt="dislike" />
-              </div>
-              <div className={styles.disable}></div>
-            </div>
-          </div>
-
-          <ul className={styles.info_use}>
-            {data.example.map((el, key) => (
-              <li className={styles.list_use} key={key}>
-                <p className={styles.text_use}>{el}</p>
-              </li>
-            ))}
+            </li>
           </ul>
+          <h4 className={styles.info_author}>Автор: {data.author}</h4>
+
+          <div className={styles.likes_func}>
+            <div className={clsx(styles.btn_action, styles.like)}>
+              <Image src={Like} alt="like" />
+            </div>
+            <div className={clsx(styles.btn_action, styles.dislike)}>
+              <Image src={DisLike} alt="dislike" />
+            </div>
+            <div className={styles.disable}></div>
+          </div>
         </div>
+
+        <ul className={styles.info_example}>
+          {data.example.map((el, key) => (
+            <li className={styles.list_example} key={key}>
+              <p className={styles.text_example}>{el}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
