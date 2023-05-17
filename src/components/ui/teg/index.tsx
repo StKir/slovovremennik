21c24f3d0d7839ba8e0d35bbd09986a71ecaabc2
Даниил Мухреев <muxreev.dan2@yandex.ru>
2023-05-17 21:55:54 +0300

    feat: :zap: Добавлен TegsList

--- src/components/ui/teg/index.tsx
+++ src/components/ui/teg/index.tsx
@@ -2,12 +2,41 @@ import clsx from 'clsx';
 import styles from './tegButton.module.scss';
 
 interface ITeg {
-	children: React.ReactElement | React.ReactNode;
-	type: 'blue' | 'pink';
+  children: React.ReactElement | React.ReactNode;
+  type: 'blue' | 'pink';
+}
+
+interface IListTegs {
+  listTegs: Array<string>;
+  differentColor?: boolean;
 }
 
 const Teg: React.FC<ITeg> = ({ children, type }) => {
-	return <div className={clsx(styles.teg, styles[type])}>{children}</div>;
+  return <div className={clsx(styles.teg, styles[type])}>{children}</div>;
+};
+
+export const ListFromTegs: React.FC<IListTegs> = ({ listTegs, differentColor = false }) => {
+  if (differentColor) {
+    return (
+      <>
+        {listTegs.map((el, index) => (
+          <Teg key={index} type={index % 2 === 0 ? 'pink' : 'blue'}>
+            {el}
+          </Teg>
+        ))}
+      </>
+    );
+  }
+
+  return (
+    <>
+      {listTegs.map((el, index) => (
+        <Teg key={index} type="blue">
+          {el}
+        </Teg>
+      ))}
+    </>
+  );
 };
 
 export default Teg;
